0823b902f82b7628acef493119794d0979d8309f
michbur <michalburdukiewicz@gmail.com>
2017-02-20 23:04:50 +0100

    stil working on figures

--- render_post.R
+++ render_post.R
@@ -1,4 +1,6 @@
 library(knitr)
 
+opts_chunk$set(fig.path = "./blog/beeswarm/figure/")
+
 knit(input = "./rmd_posts/2017-02-20-beeswarm.Rmd", 
-     output = "./_posts/articles/2017-02-20-beeswarm.md")
+     output = "./_posts/blog/2017-02-20-beeswarm.md")
