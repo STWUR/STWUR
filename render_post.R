11ac5f29fc9b57639afeb61502fda13d0cc45dd8
michbur <michalburdukiewicz@gmail.com>
2017-02-21 09:54:02 +0100

    first post done

--- render_post.R
+++ render_post.R
@@ -1,6 +1,11 @@
 library(knitr)
+library(dplyr)
 
 opts_chunk$set(fig.path = "./blog/beeswarm/figure/")
 
 knit(input = "./rmd_posts/2017-02-20-beeswarm.Rmd", 
      output = "./_posts/blog/2017-02-20-beeswarm.md")
+
+readLines("./_posts/blog/2017-02-20-beeswarm.md") %>% 
+  gsub("./blog/beeswarm/", "./", x = ., fixed = TRUE) %>% 
+  cat(file = "./_posts/blog/2017-02-20-beeswarm.md", sep = "\n")
