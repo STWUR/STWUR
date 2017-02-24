ecea693e91d5298749b469d5b00070e91b9d83ec
michbur <michalburdukiewicz@gmail.com>
2017-02-24 16:10:11 +0100

    added address to setup

--- setup_stwur.R
+++ setup_stwur.R
@@ -1,4 +1,5 @@
 # install packages and data for stwur
+# address: http://tinyurl.com/STWUR-setup
 install.packages(c("ggplot2", "dplyr"), repos = "https://cloud.r-project.org/")
 download.file(url = "https://github.com/michbur/Diagnoza_dane/archive/master.zip", 
               destfile = "diagnoza.zip")
