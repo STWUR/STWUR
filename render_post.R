88a084f29d5476d349826a4bb0a941c42a14a662
michbur <michalburdukiewicz@gmail.com>
2017-06-22 18:03:28 +0200

    the last presentation added

--- render_post.R
+++ render_post.R
@@ -18,4 +18,4 @@ render_post <- function(post_name) {
   print(paste0(output_name, " created."))
 }
 
-render_post("./rmd_posts/2017-06-08-stwur5_prezentacja.Rmd")
+render_post("./rmd_posts/2017-06-22-stwur6_prezentacja.Rmd")
